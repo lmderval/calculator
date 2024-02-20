ae484d63b3f3cc14d355f701e42e26e917c84b49
louis-mael <louismaelderval@gmail.com>
2024-02-20 18:52:23 +0100

    fix(calculator): refactor NumberASTNode and BinaryASTNode with lombok

--- ast/src/main/java/calculator/ast/NumberASTNode.java
+++ ast/src/main/java/calculator/ast/NumberASTNode.java
@@ -1,20 +1,16 @@
 package calculator.ast;
 
 import calculator.visitor.IVisitor;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@AllArgsConstructor
+@Getter
 public class NumberASTNode extends ASTNode {
     private final int value;
-
-    public NumberASTNode(int value) {
-        this.value = value;
-    }
 
     @Override
     public <T> T accept(IVisitor<T> visitor) {
         return visitor.visit(this);
-    }
-
-    public int getValue() {
-        return value;
     }
 }
